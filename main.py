d858a7ae05e50e3f2119537cf0c7d2446e49f552
Анастасия Овчаренко <A.Ovcharenko@aps-smart.com>
2017-04-12 12:34:37 +0300

    data tend

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+Created on 12 апр. 2017 г.
+
+@author: smiyan.a
+'''
+
+from Data_tend import getdic
+if __name__ == '__main__':
+    t=getdic('C:\\Users\\smiyan.a\\My Documents\\LiClipse Workspace\\Second_pr\\dic.json')
+    print(t)
